511ed0aa435a6b761458dde4d42572fde06b0212
nextchanupol <nextchanupol@gmail.com>
2020-11-07 11:14:39 +0700

    [Update] use object to return

--- check-string-method.js
+++ check-string-method.js
@@ -3,22 +3,29 @@
 // const str = 'AbCdEzXy';
 const str = 'A1b2C3d4/+*';
 
+const returnChecked = {
+  invalidInput: 'Invalid Input',
+  allCapitalLetter: 'All Capital Letter',
+  allSmallLetter: 'All Small Letter',
+  mix: 'Mix',
+}
+
 function checkString(str) {
 
   let returnString = '';
   const invlidPatern = /^[A-Za-z0-9 ]+$/
 
   if (!invlidPatern.test(str)) {
-    returnString = 'Invalid Input';
+    returnString = returnChecked.invalidInput;
     return returnString;
   }
 
   if (!/[a-z]/.test(str) && /[A-Z]/.test(str)) {
-    returnString = "All Capital Letter";
+    returnString = returnChecked.allCapitalLetter;
   } else if (/[a-z]/.test(str) && !/[A-Z]/.test(str)) {
-    returnString = "All Small Letter";
+    returnString = returnChecked.allSmallLetter;
   } else if (/[a-z]/.test(str) && /[A-Z]/.test(str)) {
-    returnString = "Mix";
+    returnString = returnChecked.mix;
   }
   return returnString;
 }
